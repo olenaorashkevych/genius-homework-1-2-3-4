45e0fd38d271b5ffae7ad6a0b2d8486aa580c0f3
Olena <olenaorashkevych@gmail.com>
2026-01-09 19:32:27 +0100

    positioning

---
+++ formats-slider.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+
+});
